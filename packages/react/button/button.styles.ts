fd38282718c605fb3ab8ed3f222f9f39b05b43fd
Kia Storm <kia@kormsen.com>
2022-01-27 11:08:06 +0000

    add real theme

--- packages/react/button/button.styles.ts
+++ packages/react/button/button.styles.ts
@@ -3,116 +3,42 @@ import { styled } from "../react-stitches";
 export const buttonVariants = {
   size: {
     "1": {
-      borderRadius: "$1",
+      borderRadius: "$2",
       height: "$8",
-      px: "$5",
-      fontSize: "$1",
+      minWidth: "$19",
+      px: "$4",
+      fontSize: "$4",
       lineHeight: "$sizes$5",
     },
     "2": {
-      borderRadius: "$1",
-      height: "$9",
-      // Figma value was not an integer so I've rounded up to the next increment in spacing system
+      borderRadius: "$2",
+      height: "$10",
+      minWidth: "97px",
       px: "$6",
       fontSize: "$2",
       lineHeight: "$sizes$6",
     },
-    "3": {
-      borderRadius: "$2",
-      height: "$10",
-      // Figma value was not an integer so I've rounded up to the next increment in spacing system
-      px: "$7",
-      fontSize: "$3",
-      lineHeight: "$sizes$7",
-    },
   },
   variant: {
-    "brand-primary": {
-      background: "linear-gradient(93.88deg, #FF00FF -44.2%, #311B92 157.32%)",
+    primary: {
+      bgc: "$purple900",
       color: "white",
       "@hover": {
         "&:hover": {
-          background:
-            "linear-gradient(0deg, $colors$whiteA2, $colors$whiteA2), linear-gradient(93.88deg, #FF00FF -44.2%, #311B92 157.32%)",
-          boxShadow: "0px 1px 4px rgba(0, 0, 0, 0.25)",
+          background: "$purple800",
         },
       },
-      "&:focus": {
-        background:
-          "linear-gradient(0deg, $colors$whiteA2, $colors$whiteA2), linear-gradient(93.88deg, #FF00FF -44.2%, #311B92 157.32%)",
+      "&:active": {
+        background: "$purple700",
         boxShadow: "0px 0px 0px 3px $colors$purple400",
       },
-      "&:active": {
-        background:
-          "linear-gradient(0deg, $colors$whiteA1, $colors$whiteA1), linear-gradient(93.88deg, #FF00FF -44.2%, #311B92 157.32%)",
-        boxShadow: "0px 0px 0px 3px $colors$purple400",
+      "&:focus": {
+        background: "$purple800",
+        boxShadow:
+          "inset 0 0 0 1px $colors$purple400, 0px 0px 0px 3px $colors$purple400",
       },
       "&:disabled": {
         background: "$colors$purple400",
-      },
-    },
-    "brand-secondary": {
-      boxShadow: "inset 0 0 0 2px $colors$purple900",
-      color: "$purple900",
-      "@hover": {
-        "&:hover": {
-          background: "$purple200",
-          boxShadow: "inset 0 0 0 2px $colors$purple900",
-        },
-      },
-      "&:focus": {
-        background: "$purple200",
-        boxShadow:
-          "inset 0 0 0 2px $colors$purple900, 0px 0px 0px 3px $colors$purple400",
-      },
-      "&:active": {
-        background: "$colors$purple400",
-        boxShadow:
-          "inset 0 0 0 2px $colors$purple900, 0px 0px 0px 3px $colors$purple400",
-      },
-      "&:disabled": {
-        color: "$purple400",
-        boxShadow: "0px 0px 0px 3px $colors$purple400",
-      },
-    },
-    "alt-primary": {
-      background:
-        "linear-gradient(276.11deg, $colors$cyan800 10.24%, #00E0F7 130.22%)",
-      color: "white",
-      "@hover": {
-        "&:hover": {
-          background:
-            "linear-gradient(0deg, $whiteA1, $whiteA1), linear-gradient(276.11deg, $colors$cyan800 10.24%, #00E0F7 130.22%)",
-          boxShadow: "0px 1px 4px rgba(0, 0, 0, 0.25)",
-        },
-      },
-      "&:focus": {
-        background:
-          "linear-gradient(276.11deg, $colors$cyan800 10.24%, #00E0F7 130.22%)",
-        boxShadow: "0px 0px 0px 3px $colors$cyan800",
-      },
-      "&:active": {
-        background:
-          "linear-gradient(0deg, $colors$whiteA1, $colors$whiteA1), linear-gradient(276.11deg, $colors$cyan800 10.24%, #00E0F7 130.22%)",
-        boxShadow: "0px  0px 0px 3px $colors$cyan800",
-      },
-      "&:disabled": {
-        background:
-          "linear-gradient(276.11deg, $colors$cyan800 10.24%, #00E0F7 130.22%)",
-        opacity: 0.4,
-      },
-    },
-    "alt-secondary": {
-      background: "white",
-      color: "$neutral800",
-      boxShadow: "0px 0px 0px 1px $colors$neutral500",
-      "&:focus": {
-        boxShadow:
-          "0px 0px 0px 1px $colors$neutral500, 0px 0px 0px 3px #E6E5EA",
-      },
-      "&:disabled": {
-        opacity: "0.4",
-        boxShadow: "none",
       },
     },
   },
@@ -139,16 +65,18 @@ export const StyledButton = styled("button", {
   // Custom
   height: "$5",
   px: "$2",
+  font: "untitled",
   fontSize: "$2",
   fontWeight: 700,
   cursor: "pointer",
+  transition: "all 0.2s ease-in-out",
 
   "&:disabled": {
     pointerEvents: "none",
   },
   variants: buttonVariants,
   defaultVariants: {
-    variant: "brand-primary",
+    variant: "primary",
     size: "1",
   },
 });
